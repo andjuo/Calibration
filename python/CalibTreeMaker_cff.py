b46d72c2c92d10b31b8b98bc35e7ccd63386e83f
auterman <>
2008-11-12 16:15:10 +0000

    Updating CalibTreeMaker python config files to use Top, too.

--- python/CalibTreeMaker_cff.py
+++ python/CalibTreeMaker_cff.py
@@ -4,6 +4,7 @@ from Calibration.CalibSamples.PhotonJetSample_cff import *
 from Calibration.CalibSamples.TrackTowerSample_cff import *
 from Calibration.CalibSamples.DiJetSample_cff import *
 from Calibration.CalibSamples.TriJetSample_cff import *
+from Calibration.CalibSamples.TopSample_cff import *
 from Calibration.CalibTreeMaker.CalibTreeMaker_cfi import *
 photonJetInput = cms.Sequence(selectedPhotonJetSample)
 trackTowerInput = cms.Sequence(selectedTrackTowerSample)
@@ -14,5 +15,6 @@ makeTrackTowerTree = cms.Sequence(trackTowerInput*calibTreeMaker)
 makeDiJetTree = cms.Sequence(DiJetInput*calibTreeMaker)
 makeTriJetTree = cms.Sequence(TriJetInput*calibTreeMaker)
 makeNJetTree = cms.Sequence(DiJetInput+TriJetInput*calibTreeMaker)
-makeAllTrees = cms.Sequence(photonJetInput+trackTowerInput+DiJetInput+TriJetInput*calibTreeMaker)
+makeTopTree = cms.Sequence(TopInput*calibTreeMaker)
+makeAllTrees = cms.Sequence(photonJetInput+trackTowerInput+DiJetInput+TriJetInput+TopInput*calibTreeMaker)
 
