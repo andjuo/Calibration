ed160bf039101c23fdd8dbdde4a2b4936b35c961
snaumann <>
2009-02-12 19:54:50 +0000

    follow some renamings in the TQAF and run with CMSSW 2_2_4 on Summer08 sample

--- python/CalibTreeMaker_Top_cff.py
+++ python/CalibTreeMaker_Top_cff.py
@@ -3,6 +3,6 @@ import FWCore.ParameterSet.Config as cms
 from Calibration.CalibSamples.TopSample_cff import *
 from Calibration.CalibTreeMaker.CalibTreeMaker_cfi import *
 
-TopInput           = cms.Sequence(makeTopSample_semiLepFilter)
+TopInput           = cms.Sequence(makeTopSample_semiLepFilter_aod)
 
 makeTopTree        = cms.Sequence(TopInput*calibTreeMaker)
