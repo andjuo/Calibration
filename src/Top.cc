0858961dc2b42b16ff8191450a7bc7247bbf099e
snaumann <>
2009-07-13 07:42:43 +0000

    1.) write jet correction factors L1, L2, L3, L4, L5 into the tree; 2.) use new member functions to restructure the code for both b-jets and W-jets:     fillRecJet, fillTowers, fillCorrFactors, fillGenJet 3.) use edm::View<reco::Jet> in order to make this code work with reco::CaloJet and pat::Jet     (note: the jetCorrFactors are currently only filled for pat::Jet)

--- src/Top.cc
+++ src/Top.cc
@@ -1,5 +1,4 @@
 #include "Calibration/CalibTreeMaker/interface/Top.h"
-#include "DataFormats/METReco/interface/CaloMET.h"
 
 #include "DataFormats/Math/interface/deltaR.h"
 
@@ -11,7 +10,7 @@ void Top::setup(const edm::ParameterSet& cfg, TTree* CalibTree)
   weight     = (float)(cfg.getParameter<double>("Top_Weight"));
   weight_tag = cfg.getParameter<edm::InputTag> ("Top_Weight_Tag");
 
-  //tower for all -jets
+  // CaloTowers
   int kMAX = 10000;
   towet  = new float [ kMAX ];
   toweta = new float [ kMAX ];
@@ -24,7 +23,6 @@ void Top::setup(const edm::ParameterSet& cfg, TTree* CalibTree)
   towid_eta = new int[ kMAX ];
   towid     = new int[ kMAX ];
   tow_jetidx= new int[ kMAX ];
-  // CaloTower Branches for -Jets
   CalibTree->Branch( "NobjTow", &NobjTow,"NobjTow/I"             );
   CalibTree->Branch( "TowId",     towid,      "TowId[NobjTow]/I"    );
   CalibTree->Branch( "TowId_phi", towid_phi,  "TowId_phi[NobjTow]/I");
@@ -37,7 +35,7 @@ void Top::setup(const edm::ParameterSet& cfg, TTree* CalibTree)
   CalibTree->Branch( "TowHad",    towhd,      "TowHad[NobjTow]/F"   );
   CalibTree->Branch( "TowOE",     towoe,      "TowOE[NobjTow]/F"    );
   CalibTree->Branch( "Tow_jetidx",tow_jetidx, "Tow_jetidx[NobjTow]/I");
-  //All -jets
+  // RecoJets
   int kjMAX = 8;
   jetpt  = new float [ kjMAX ];
   jetphi = new float [ kjMAX ];
@@ -46,15 +44,31 @@ void Top::setup(const edm::ParameterSet& cfg, TTree* CalibTree)
   jete   = new float [ kjMAX ];
   jetflavor = new int[ kjMAX ];
   jettopid  = new int[ kjMAX ];
-  // -Jet-specific ranches of the tree 
+  jscaleL1  = new float[ kjMAX ];
+  jscaleL2  = new float[ kjMAX ];
+  jscaleL3  = new float[ kjMAX ];
+  jscaleL4  = new float[ kjMAX ];
+  jscaleL5  = new float[ kjMAX ];
+  for(int i = 0; i < kjMAX; i++) {
+    jscaleL1[i] = 1.;
+    jscaleL2[i] = 1.;
+    jscaleL3[i] = 1.;
+    jscaleL4[i] = 1.;
+    jscaleL5[i] = 1.;
+  }
   CalibTree->Branch( "NobjJet",&NobjJet,"NobjJet/I"     );
   CalibTree->Branch( "JetPt", jetpt, "JetPt[NobjJet]/F" );
   CalibTree->Branch( "JetPhi",jetphi,"JetPhi[NobjJet]/F");
   CalibTree->Branch( "JetEta",jeteta,"JetEta[NobjJet]/F");
   CalibTree->Branch( "JetEt", jetet, "JetEt[NobjJet]/F" );
   CalibTree->Branch( "JetE",  jete,  "JetE[NobjJet]/F"  );
-  CalibTree->Branch( "JetFlavor", jetflavor,  "JetFlavor[NobjJet]/I" );
-  CalibTree->Branch( "JetTopID",  jettopid,   "JetTopID[NobjJet]/I"  );
+  CalibTree->Branch( "JetFlavor", jetflavor, "JetFlavor[NobjJet]/I" );
+  CalibTree->Branch( "JetTopID",  jettopid,  "JetTopID[NobjJet]/I"  );
+  CalibTree->Branch( "JetCorrL1", jscaleL1,  "JetCorrL1[NobjJet]/F" );
+  CalibTree->Branch( "JetCorrL2", jscaleL2,  "JetCorrL2[NobjJet]/F" );
+  CalibTree->Branch( "JetCorrL3", jscaleL3,  "JetCorrL3[NobjJet]/F" );
+  CalibTree->Branch( "JetCorrL4", jscaleL4,  "JetCorrL4[NobjJet]/F" );
+  CalibTree->Branch( "JetCorrL5", jscaleL5,  "JetCorrL5[NobjJet]/F" );
   // GenJets
   genjetpt  = new float [ kjMAX ];
   genjetphi = new float [ kjMAX ];
@@ -81,141 +95,172 @@ void Top::analyze(const edm::Event& evt, const edm::EventSetup& setup, TTree* Ca
     weight = (float)( *weightHandle );
   }
 
-  edm::Handle<reco::CaloJetCollection> pBJets;
-  //edm::Handle<edm::View<reco::Jet> > pBJets;
+  edm::Handle<edm::View<reco::Jet> > pBJets;
   evt.getByLabel(bjets_, pBJets);
 
-  edm::Handle<reco::CaloJetCollection> pWJets;
-  //edm::Handle<edm::View<reco::Jet> > pWJets;
+  edm::Handle<edm::View<reco::Jet> > pWJets;
   evt.getByLabel(wjets_, pWJets);
 
   edm::Handle<reco::GenJetCollection> genJets;
   evt.getByLabel(genjets_, genJets);
 
-  //Need 2*n W-jets and n B-jets, where n is the number of hypotheses
+  // need 2*n W-jets and n B-jets, where n is the number of hypotheses
   if (pWJets->size()%2!=0 || pWJets->size()%pBJets->size()!=0)
     return;
 
-  //Filling W-jets
-  NobjTow=0;
+  NobjTow = 0;
   NobjJet = pWJets->size() + pBJets->size();
   unsigned int towno = 0;
-  for (unsigned int jtno = 0; jtno<pWJets->size(); ++jtno)
-  {
-    jetpt [ jtno ] = (*pWJets)[jtno].pt();
-    jetphi[ jtno ] = (*pWJets)[jtno].phi();
-    jeteta[ jtno ] = (*pWJets)[jtno].eta();
-    jetet [ jtno ] = (*pWJets)[jtno].et();
-    jete  [ jtno ] = (*pWJets)[jtno].energy();
-    jete  [ jtno ] = (*pWJets)[jtno].energy();
-    jetflavor[jtno]= 1;//uds
-    jettopid [jtno]= jtno/2;
 
-    double gjpt = 0;
-    double gjphi = 0;
-    double gjeta = 0;
-    double gjet = 0;
-    double gje = 0;
-    double DeltaR = 1000;
-    double DeltaRtemp = 0;
+  // filling W-jets
 
-    for( reco::GenJetCollection::const_iterator genJet = genJets->begin(); genJet != genJets->end(); ++genJet)
-      {
-	DeltaRtemp = deltaR(genJet->eta(), genJet->phi(),
-			    (*pWJets)[jtno].eta(), (*pWJets)[jtno].phi() );
-	if(DeltaRtemp < DeltaR)
-	  {
-	    DeltaR = DeltaRtemp;
-	    gjpt  = genJet->pt();
-	    gjphi = genJet->phi();
-	    gjeta = genJet->eta();
-	    gjet  = genJet->et();
-	    gje   = genJet->energy();
-	  }
-      }
-    genjetpt [ jtno ] = gjpt;
-    genjetphi[ jtno ] =	gjphi;
-    genjeteta[ jtno ] =	gjeta;
-    genjetet [ jtno ] =	gjet;
-    genjete  [ jtno ] = gje;
-    
-    std::vector<CaloTowerPtr> j_towers = (*pWJets)[jtno].getCaloConstituents();
-    NobjTow+=j_towers.size();
-    for (std::vector<CaloTowerPtr>::const_iterator tow = j_towers.begin(); 
-	 tow != j_towers.end(); ++tow, ++towno){
+  for(unsigned int jtno = 0; jtno<pWJets->size(); ++jtno) {
 
-      towet[towno]     = (*tow)->et();
-      toweta[towno]    = (*tow)->eta();
-      towphi[towno]    = (*tow)->phi();
-      towen[towno]     = (*tow)->energy();
-      towem[towno]     = (*tow)->emEnergy();
-      towhd[towno]     = (*tow)->hadEnergy();
-      towoe[towno]     = (*tow)->outerEnergy();
-      towid_phi[towno] = (*tow)->id().iphi();
-      towid_eta[towno] = (*tow)->id().ieta();
-      towid[towno]     = (*tow)->id().rawId();
-      tow_jetidx[towno]= jtno;
+    fillRecJet( (*pWJets)[jtno], jtno, 1, jtno/2 );
+
+    // GenJets for W-jets
+
+    fillGenJet(*genJets, (*pWJets)[jtno], jtno);
+
+    if( dynamic_cast<const reco::CaloJet*>( &((*pWJets)[jtno]) ) ) {
+
+      // towers for W-jets from CaloJets
+
+      std::vector<CaloTowerPtr> j_towers = ( dynamic_cast<const reco::CaloJet*>( &((*pWJets)[jtno]) ) )->getCaloConstituents();
+      NobjTow+=j_towers.size();
+      fillTowers(j_towers, towno, jtno);
+
     }
+    else if( dynamic_cast<const pat::Jet*>( &((*pWJets)[jtno]) ) ) {
+
+      // towers and corrFactors for W-jets from patJets
+
+      const pat::Jet* patJet = dynamic_cast<const pat::Jet*>( &((*pWJets)[jtno]) );
+
+      std::vector<CaloTowerPtr> j_towers = patJet->getCaloConstituents();
+      NobjTow+=j_towers.size();
+      fillTowers(j_towers, towno, jtno);
+
+      fillCorrFactors(*patJet, jtno, "uds");
+
+    }
+
   }
 
-  //Filling b-jets
-  for (unsigned int jtno = pWJets->size(); 
-       jtno<pBJets->size()+pWJets->size(); ++jtno)
-  {
-    jetpt [ jtno ] = (*pBJets)[jtno-pWJets->size()].pt();
-    jetphi[ jtno ] = (*pBJets)[jtno-pWJets->size()].phi();
-    jeteta[ jtno ] = (*pBJets)[jtno-pWJets->size()].eta();
-    jetet [ jtno ] = (*pBJets)[jtno-pWJets->size()].et();
-    jete  [ jtno ] = (*pBJets)[jtno-pWJets->size()].energy();
-    jetflavor[jtno]= 3;//b
-    jettopid [jtno]= jtno - pWJets->size();
+  // filling b-jets
 
-    double gjpt = 0;
-    double gjphi = 0;
-    double gjeta = 0;
-    double gjet = 0;
-    double gje = 0;
-    double DeltaR = 1000;
-    double DeltaRtemp = 0;
+  for(unsigned int jtno = pWJets->size(); 
+      jtno<pBJets->size()+pWJets->size(); ++jtno) {
 
-    for( reco::GenJetCollection::const_iterator genJet = genJets->begin(); genJet != genJets->end(); ++genJet)
-      {
-	DeltaRtemp = deltaR(genJet->eta(), genJet->phi(),
-			    (*pBJets)[jtno-pWJets->size()].eta() , (*pBJets)[jtno-pWJets->size()].phi() );
-	if(DeltaRtemp < DeltaR)
-	  {
-	    DeltaR = DeltaRtemp;
-	    gjpt  = genJet->pt();
-	    gjphi = genJet->phi();
-	    gjeta = genJet->eta();
-	    gjet  = genJet->et();
-	    gje   = genJet->energy();
-	  }
-      }
-    genjetpt [ jtno ] = gjpt;
-    genjetphi[ jtno ] =	gjphi;
-    genjeteta[ jtno ] =	gjeta;
-    genjetet [ jtno ] =	gjet;
-    genjete  [ jtno ] = gje;
+    fillRecJet( (*pBJets)[jtno-pWJets->size()], jtno, 3, jtno-pWJets->size() );
 
-    std::vector<CaloTowerPtr> j_towers = (*pBJets)[jtno-pWJets->size()].getCaloConstituents();
-    NobjTow+=j_towers.size();
-    for (std::vector<CaloTowerPtr>::const_iterator tow = j_towers.begin(); 
-	 tow != j_towers.end(); ++tow, ++towno){
+    // GenJets for b-jets
 
-      towet[towno]     = (*tow)->et();
-      toweta[towno]    = (*tow)->eta();
-      towphi[towno]    = (*tow)->phi();
-      towen[towno]     = (*tow)->energy();
-      towem[towno]     = (*tow)->emEnergy();
-      towhd[towno]     = (*tow)->hadEnergy();
-      towoe[towno]     = (*tow)->outerEnergy();
-      towid_phi[towno] = (*tow)->id().iphi();
-      towid_eta[towno] = (*tow)->id().ieta();
-      towid[towno]     = (*tow)->id().rawId();
-      tow_jetidx[towno]= jtno;
+    fillGenJet(*genJets, (*pBJets)[jtno-pWJets->size()], jtno);
+
+    if( dynamic_cast<const reco::CaloJet*>( &((*pBJets)[jtno-pWJets->size()]) ) ) {
+    
+      // towers for b-jets from CaloJets
+
+      std::vector<CaloTowerPtr> j_towers = ( dynamic_cast<const reco::CaloJet*>( &((*pBJets)[jtno-pWJets->size()]) ) )->getCaloConstituents();
+      NobjTow+=j_towers.size();
+      fillTowers(j_towers, towno, jtno);
+
     }
+    else if( dynamic_cast<const pat::Jet*>( &((*pBJets)[jtno-pWJets->size()]) ) ) {
+
+      // towers and corrFactors for b-jets from patJets
+
+      const pat::Jet* patJet = dynamic_cast<const pat::Jet*>( &((*pBJets)[jtno-pWJets->size()]) );
+
+      std::vector<CaloTowerPtr> j_towers = patJet->getCaloConstituents();
+      NobjTow+=j_towers.size();
+      fillTowers(j_towers, towno, jtno);
+
+      fillCorrFactors(*patJet, jtno, "b");
+
+    }
+
   }
 
   CalibTree->Fill();
+}
+
+void Top::fillRecJet(const reco::Jet& recJet, const unsigned int jtno, const unsigned int flavor, const unsigned int topid) {
+
+  jetpt    [jtno] = recJet.pt();
+  jetphi   [jtno] = recJet.phi();
+  jeteta   [jtno] = recJet.eta();
+  jetet    [jtno] = recJet.et();
+  jete     [jtno] = recJet.energy();
+  jetflavor[jtno] = flavor; // 1=uds, 3=b
+  jettopid [jtno] = topid;
+
+}
+
+void Top::fillTowers(const std::vector<CaloTowerPtr> &towers, unsigned int& towno, const unsigned int jtno) {
+
+  for(std::vector<CaloTowerPtr>::const_iterator tow = towers.begin(); 
+      tow != towers.end(); ++tow, ++towno){
+
+    towet     [towno] = (*tow)->et();
+    toweta    [towno] = (*tow)->eta();
+    towphi    [towno] = (*tow)->phi();
+    towen     [towno] = (*tow)->energy();
+    towem     [towno] = (*tow)->emEnergy();
+    towhd     [towno] = (*tow)->hadEnergy();
+    towoe     [towno] = (*tow)->outerEnergy();
+    towid_phi [towno] = (*tow)->id().iphi();
+    towid_eta [towno] = (*tow)->id().ieta();
+    towid     [towno] = (*tow)->id().rawId();
+    tow_jetidx[towno] = jtno;
+
+  }
+
+}
+
+void Top::fillCorrFactors(const pat::Jet &jet, const unsigned int jtno, const std::string &flavor) {
+
+  if( jet.hasCorrFactors() ) {
+    jscaleL1[jtno] = jet.corrFactor("off");
+    jscaleL2[jtno] = jet.corrFactor("rel");
+    jscaleL3[jtno] = jet.corrFactor("abs");
+    jscaleL4[jtno] = jet.corrFactor("emf");
+    jscaleL5[jtno] = jet.corrFactor("had", flavor);
+  }
+
+}
+
+void Top::fillGenJet(const reco::GenJetCollection &genJets, const reco::Jet& recJet, const unsigned int jtno) {
+  
+  double gjpt   = 0;
+  double gjphi  = 0;
+  double gjeta  = 0;
+  double gjet   = 0;
+  double gje    = 0;
+  double dR     = 1000;
+  double dRtemp = 0;
+
+  for(reco::GenJetCollection::const_iterator genJet = genJets.begin(); genJet != genJets.end(); ++genJet) {
+
+    dRtemp = deltaR(genJet->eta(), genJet->phi(),
+		    recJet.eta() , recJet.phi() );
+
+    if(dRtemp < dR) {
+      dR    = dRtemp;
+      gjpt  = genJet->pt();
+      gjphi = genJet->phi();
+      gjeta = genJet->eta();
+      gjet  = genJet->et();
+      gje   = genJet->energy();
+    }
+
+  }
+
+  genjetpt [jtno] = gjpt;
+  genjetphi[jtno] = gjphi;
+  genjeteta[jtno] = gjeta;
+  genjetet [jtno] = gjet;
+  genjete  [jtno] = gje;
+
 }
