27866fddebeb31be23922a0c5995307e146199fd
snaumann <>
2009-07-28 12:46:03 +0000

    * use TtSemiLepJetPartonMatch plugin in the TQAF for genJet-parton matching; * use new python function in CalibSamples.TopFilter_cff to switch on event   selection cuts; * choose sampleType for L5 corrections; * use redigi version of input sample

--- test/testCalibTreeMaker_Top_cfg.py
+++ test/testCalibTreeMaker_Top_cfg.py
@@ -2,43 +2,27 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("Calib")
 
-## add message logger
+## Add message logger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
 process.MessageLogger.cerr.threshold = 'INFO'
 process.MessageLogger.categories.append('TtSemiLeptonicEvent')
 process.MessageLogger.categories.append('JetPartonMatching')
 process.MessageLogger.cerr.TtSemiLeptonicEvent = cms.untracked.PSet(
-    limit = cms.untracked.int32(10)
+    limit = cms.untracked.int32(0)
 )
 process.MessageLogger.cerr.JetPartonMatching = cms.untracked.PSet(
-    limit = cms.untracked.int32(10)
+    limit = cms.untracked.int32(0)
 )
 
-#process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 #
 # Define input source
 #
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(
-    #
-    # TTbar sample 1 (100 events)
-    #
-    #'file:/afs/cern.ch/cms/PRS/top/cmssw-data/relval200-for-pat-testing/FullSimTTBar-2_1_X_2008-07-08_STARTUP_V4-AODSIM.100.root'
-    # TTbar sample 2
-    #'/store/relval/CMSSW_2_1_9/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/STARTUP_V7_v2/0000/16D75F0F-1186-DD11-80B9-000423D98C20.root',
-    #'/store/relval/CMSSW_2_1_9/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/STARTUP_V7_v2/0000/24CD41BB-1A86-DD11-9CDA-000423D98EC8.root',
-    #'/store/relval/CMSSW_2_1_9/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/STARTUP_V7_v2/0000/264D79AA-1786-DD11-9F3C-001617C3B6DC.root',
-    #'/store/relval/CMSSW_2_1_9/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/STARTUP_V7_v2/0000/327DE1B9-0E86-DD11-B7B1-000423D6C8E6.root',
-    #'/store/relval/CMSSW_2_1_9/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/STARTUP_V7_v2/0000/345AE083-1186-DD11-8D43-000423D99658.root',
-    #'/store/relval/CMSSW_2_1_9/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/STARTUP_V7_v2/0000/4A0ADB7D-1086-DD11-BD16-000423D98E6C.root',
-    #'/store/relval/CMSSW_2_1_9/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/STARTUP_V7_v2/0000/4E31E969-1886-DD11-8398-000423D9989E.root',
-    #'/store/relval/CMSSW_2_1_9/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/STARTUP_V7_v2/0000/4EEEA6AE-0886-DD11-90F9-000423D94990.root'
-    ## Summer08 TauolaTTBar at DESY (just one file of the sample)
-    #'/store/mc/Summer08/TauolaTTbar/GEN-SIM-RECO/IDEAL_V9_v2/0009/0054812D-26F7-DD11-99D7-001F2908F0E4.root'
-    ## Fall08 TTJets-madgraph at DESY (just one file of the sample)
-   '/store/mc/Fall08/TTJets-madgraph/GEN-SIM-RECO/IDEAL_V9_v2/0000/1E3EC51A-CEED-DD11-8353-001F29086E48.root'
+    '/store/mc/Fall08/TTJets-madgraph/GEN-SIM-RECO/IDEAL_V11_redigi_v10/0010/B493FB91-EA00-DE11-B852-00E081791899.root'
     )
 )
 
@@ -56,20 +40,34 @@ process.load("Configuration.StandardSequences.FakeConditions_cff")
 # Configuration for Top
 #
 process.load("Calibration.CalibTreeMaker.CalibTreeMaker_Top_cff")
-process.calibTreeMaker.TopTreeName    = 'TopTree'
-process.calibTreeMaker.TopHadBJets    = 'topSample:TopHadBJets'
-process.calibTreeMaker.TopHadWJets    = 'topSample:TopHadWJets'
-process.calibTreeMaker.Top_GenJets    = 'sisCone5GenJets'
-process.calibTreeMaker.Top_Weight_Tag = 'tag'
-process.calibTreeMaker.Top_Weight     = 1.
 process.ttSemiLepJetPartonMatch.algorithm  = 'unambiguousOnly'
 process.ttSemiLepJetPartonMatch.useMaxDist = True
 process.ttSemiLepJetPartonMatch.maxDist    = 0.3
 process.ttSemiLepJetPartonMatch.maxNJets   = 5
 process.ttSemiLepJetPartonMatch.partonsToIgnore = ["HadB", "LepB"]
-# For debugging
-process.ttSemiLepJetPartonMatch.verbosity = 1
+process.ttSemiLepGenJetPartonMatch.algorithm  = "unambiguousOnly"
+process.ttSemiLepGenJetPartonMatch.useMaxDist = True
+process.ttSemiLepGenJetPartonMatch.maxDist    = 0.3
+process.ttSemiLepGenJetPartonMatch.maxNJets   = 5
+process.ttSemiLepGenJetPartonMatch.partonsToIgnore = ["HadB", "LepB"]
+
+#
+# Printout for debugging
+#
+process.ttSemiLepJetPartonMatch.verbosity = 100
+process.ttSemiLepGenJetPartonMatch.verbosity = 100
 process.ttSemiLepEvent.verbosity = 1
+
+#
+# Turn on event selection cuts
+#
+from Calibration.CalibSamples.TopFilter_cff import setupEventSelection
+setupEventSelection()
+
+#
+# Choose sampleType for L5 corrections
+#
+process.jetCorrFactors.sampleType = "ttbar" # dijet or ttbar
 
 #
 # Switch jet collection to sisCone5CaloJets
@@ -95,12 +93,6 @@ process.calibTreeMaker.WritePhotonJetTree = False
 process.calibTreeMaker.WriteDiJetTree     = False
 process.calibTreeMaker.WriteTriJetTree    = False
 process.calibTreeMaker.WriteTopTree       = True
-
-## necessary fixes to run 2.2.X on 2.1.X data
-## comment this when running on samples produced
-## with 22X
-from PhysicsTools.PatAlgos.tools.cmsswVersionTools import run22XonSummer08AODSIM
-run22XonSummer08AODSIM(process)
 
 #
 # The path
