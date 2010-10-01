8619542984e367b8009475a1daffd70d5a14ebb4
stadie <>
2010-10-01 13:56:19 +0000

    config file for 38x data and submission with grid-control

--- test/makeDiJetsData_cfg.py
+++ test/makeDiJetsData_cfg.py
@@ -5,7 +5,7 @@ process = cms.Process("Calib")
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.threshold             = 'INFO'
-process.MessageLogger.cerr.FwkReport.reportEvery = 100
+process.MessageLogger.cerr.FwkReport.reportEvery = 5000
 
 
 process.source = cms.Source("PoolSource",
@@ -13,7 +13,8 @@ process.source = cms.Source("PoolSource",
     #'/store/data/Run2010A/JetMETTau/RECO/v1/000/136/082/147EC62F-DF66-DF11-9678-003048D2BE08.root'
     #'/store/data/Commissioning10/MinimumBias/RECO/May6thPDSkim2_SD_JetMETTau-v1/0137/FE3CCCB6-D05D-DF11-BB30-00261894383F.root'
     # '/store/data/Commissioning10/MinimumBias/RECO/GOODCOLL-Jun9thSkim_v1/0039/20C793BD-3678-DF11-AF29-0018F3D09706.root'
-    '/store/data/Run2010A/JetMET/RECO/v4/000/141/960/48753CF6-D39B-DF11-AB95-0030487A18F2.root'
+    # '/store/data/Run2010A/JetMET/RECO/v4/000/141/960/48753CF6-D39B-DF11-AB95-0030487A18F2.root'
+    '/store/data/Run2010A/JetMET/RECO/Sep17ReReco_v2/0024/7E51025E-18C7-DF11-BEA3-0018F3D0962A.root'
             )
                             )
 
@@ -35,7 +36,7 @@ process.hltLevel1GTSeed.L1SeedsLogicalExpression = cms.string('0 AND (40 OR 41) 
 # HLT
 process.load('HLTrigger.HLTfilters.hltHighLevel_cfi')
 #process.hltHighLevel.HLTPaths = ('HLT_DiJetAve15U','HLT_DiJetAve30U','HLT_DiJetAve50U')
-process.hltHighLevel.HLTPaths = cms.vstring('HLT_DiJetAve15U')
+process.hltHighLevel.HLTPaths = cms.vstring('HLT_DiJetAve50U')
 process.hltHighLevel.andOr = cms.bool(True)
 process.hltHighLevel.throw = cms.bool(True)
 
@@ -67,7 +68,7 @@ process.load("TrackingTools.TrackAssociator.DetIdAssociatorESProducer_cff")
 process.load("TrackingTools.TrackAssociator.default_cfi")
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = 'GR_R_38X_V13::All'
+process.GlobalTag.globaltag = 'GR_R_38X_V13A::All'
 
 
 process.load("Calibration.CalibTreeMaker.CalibTreeMaker_cfi")
@@ -83,7 +84,7 @@ process.load("RecoJets.Configuration.RecoGenJets_cff")
 
 # Jet Energy Corrections
 process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
-# L4JW Correction Services
+# L4JW and L2L3Residual Correction Services
 # you need special versions of CondFormats and JetMETCorrections:
 # copy the following dirs to your release (this works with 3_8_4):
 # /afs/cern.ch/user/s/stadie/public/CondFormats
@@ -96,11 +97,12 @@ process.ak5CaloL4JW = cms.ESSource(
     level     = cms.string('L4JW'),
     algorithm = cms.string('AK5Calo'),
     useCondDB = cms.untracked.bool(False)
-    )
+)
 process.ak5CaloL2L3L4JW = cms.ESSource(
     'JetCorrectionServiceChain',
-    correctors = cms.vstring('ak5CaloL2Relative','ak5CaloL3Absolute','ak5CaloL4JW')
+    correctors = cms.vstring('ak5CaloL2L3Residual','ak5CaloL4JW')
 )
+
 # ZSP and JPT corrections
 process.load("RecoJets.Configuration.RecoJPTJets_cff")
 
@@ -118,7 +120,7 @@ process.calibTreeMakerCalo.NJetZSPJets       = 'ZSPJetCorJetAntiKt5'
 process.calibTreeMakerCalo.NJet_L2JetCorrector      = cms.string('ak5CaloL2Relative')
 process.calibTreeMakerCalo.NJet_L3JetCorrector      = cms.string('ak5CaloL3Absolute')
 process.calibTreeMakerCalo.NJet_JPTZSPCorrector     = cms.string('JetPlusTrackZSPCorrectorAntiKt5')
-process.calibTreeMakerCalo.NJet_L2L3JetCorrector    = cms.string('ak5CaloL2L3')
+process.calibTreeMakerCalo.NJet_L2L3JetCorrector    = cms.string('ak5CaloL2L3Residual')
 process.calibTreeMakerCalo.NJet_L2L3L4JWJetCorrector    = cms.string('ak5CaloL2L3L4JW')
 process.calibTreeMakerCalo.NJet_L2L3JetCorrectorJPT = cms.string('ak5JPTL2L3')
 process.calibTreeMakerCalo.NJetConeSize      = 0.5
@@ -133,9 +135,9 @@ process.calibTreeMakerPF.NJetZSPJets       = 'ZSPJetCorJetAntiKt5'
 process.calibTreeMakerPF.NJet_L2JetCorrector      = cms.string('ak5PFL2Relative')
 process.calibTreeMakerPF.NJet_L3JetCorrector      = cms.string('ak5PFL3Absolute')
 process.calibTreeMakerPF.NJet_JPTZSPCorrector     = cms.string('JetPlusTrackZSPCorrectorAntiKt5')
-process.calibTreeMakerPF.NJet_L2L3JetCorrector    = cms.string('ak5PFL2L3')
-process.calibTreeMakerPF.NJet_L2L3L4JWJetCorrector    = cms.string('ak5PFL2L3')
-process.calibTreeMakerPF.NJet_L2L3JetCorrectorJPT = cms.string('ak5JPT')
+process.calibTreeMakerPF.NJet_L2L3JetCorrector    = cms.string('ak5PFL2L3Residual')
+process.calibTreeMakerPF.NJet_L2L3L4JWJetCorrector    = cms.string('ak5PFL2L3Residual')
+process.calibTreeMakerPF.NJet_L2L3JetCorrectorJPT = cms.string('ak5JPTL2L3Residual')
 process.calibTreeMakerPF.NJetConeSize      = 0.5
 
 
@@ -153,7 +155,7 @@ process.calibTreeMakerTrack.NJet_L3JetCorrector      = cms.string('ak5TrackL3Abs
 process.calibTreeMakerTrack.NJet_JPTZSPCorrector     = cms.string('JetPlusTrackZSPCorrectorAntiKt5')
 process.calibTreeMakerTrack.NJet_L2L3JetCorrector    = cms.string('ak5TrackL2L3')
 process.calibTreeMakerTrack.NJet_L2L3L4JWJetCorrector    = cms.string('ak5TrackL2L3')
-process.calibTreeMakerTrack.NJet_L2L3JetCorrectorJPT = cms.string('ak5JPTL2L3')
+process.calibTreeMakerTrack.NJet_L2L3JetCorrectorJPT = cms.string('ak5JPTL2L3Residual')
 process.calibTreeMakerTrack.NJetConeSize      = 0.5
 
 #ATTENTION: use CALOcorrections for the time being
@@ -167,9 +169,9 @@ process.calibTreeMakerJPT.NJetZSPJets     = 'ZSPJetCorJetAntiKt5'
 process.calibTreeMakerJPT.NJet_L2JetCorrector      = cms.string('ak5JPTL2Relative')
 process.calibTreeMakerJPT.NJet_L3JetCorrector      = cms.string('ak5JPTL3Absolute')
 process.calibTreeMakerJPT.NJet_JPTZSPCorrector     = cms.string('JetPlusTrackZSPCorrectorAntiKt5')
-process.calibTreeMakerJPT.NJet_L2L3JetCorrector    = cms.string('ak5JPTL2L3')
-process.calibTreeMakerJPT.NJet_L2L3L4JWJetCorrector    = cms.string('ak5JPTL2L3')
-process.calibTreeMakerJPT.NJet_L2L3JetCorrectorJPT = cms.string('ak5JPTL2L3')
+process.calibTreeMakerJPT.NJet_L2L3JetCorrector    = cms.string('ak5JPTL2L3Residual')
+process.calibTreeMakerJPT.NJet_L2L3L4JWJetCorrector    = cms.string('ak5JPTL2L3Residual')
+process.calibTreeMakerJPT.NJet_L2L3JetCorrectorJPT = cms.string('ak5JPTL2L3Residual')
 process.calibTreeMakerJPT.NJetConeSize      = 0.5
 
 
@@ -183,8 +185,8 @@ process.pData = cms.Path( process.hltLevel1GTSeed
                           #* process.ZSPrecoJetAssociationsAntiKt5
                           * process.calibTreeMakerCalo
                           * process.calibTreeMakerPF
-                          #* process.calibTreeMakerTrack
-                          #* process.calibTreeMakerJPT
+                          * process.calibTreeMakerTrack
+                          * process.calibTreeMakerJPT
                           )
 
 process.schedule = cms.Schedule(process.pData)
