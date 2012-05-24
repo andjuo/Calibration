ffd2d01ac0e0166e3f82d9628a0fb5ac087b3ea7
mschrode <>
2012-05-24 11:41:17 +0000

    2012 data

--- test/resPhotonJetData_cfg.py
+++ test/resPhotonJetData_cfg.py
@@ -1,4 +1,4 @@
-# $Id: resPhotonJetData_cfg.py,v 1.1 2012/05/10 12:09:40 mschrode Exp $
+# $Id: resPhotonJetData_cfg.py,v 1.2 2012/05/14 17:29:07 mschrode Exp $
 #
 # Write Photon+Jet-Trees in data
 
@@ -15,12 +15,12 @@ runTreeMakerPhotonJets(
     'HLT_Photon30_CaloIdVL_IsoL_v*',
     'HLT_Photon50_CaloIdVL_IsoL_v*',
     'HLT_Photon75_CaloIdVL_IsoL_v*',
-    'HLT_Photon90_CaloIdVL_IsoL_v*'
+    'HLT_Photon90_CaloIdVL_IsoL_v*',
     'HLT_Photon135_v*',
     'HLT_Photon150_v*',
-    'HLT_Photon160_v*',
+    'HLT_Photon160_v*'
     ],
     reportEveryEvt=5000,
     testFileName="/store/data/Run2012A/Photon/AOD/PromptReco-v1/000/193/575/E83BCD15-F499-E111-B51C-003048D2BC52.root",
-    numProcessedEvt=-10
+    numProcessedEvt=-1000
     )
--- test/resPhotonJetMC_cfg.py
+++ test/resPhotonJetMC_cfg.py
@@ -1,4 +1,4 @@
-# $Id: $
+# $Id: resPhotonJetMC_cfg.py,v 1.1 2012/05/10 12:09:40 mschrode Exp $
 #
 # Write Photon+Jet-Trees in data
 
@@ -9,9 +9,9 @@ from Calibration.CalibTreeMaker.runTreeMakerPhotonJets_cff import runTreeMakerPh
 runTreeMakerPhotonJets(
     process,
     isData=False,
-    globalTag="START44_V13::All", # Fall11
+    globalTag="START52_V9::All", # Summer12
     hltSelection=[],
     reportEveryEvt=5000,
-    testFileName="/store/mc/Fall11/G_Pt-15to3000_TuneZ2_Flat_7TeV_pythia6/AODSIM/PU_S6_START44_V9B-v1/0000/0087C9E4-C03F-E111-A9A5-00261894388A.root",
+    testFileName="",
     numProcessedEvt=-100
     )
