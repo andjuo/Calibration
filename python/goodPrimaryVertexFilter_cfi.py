8b72d2ff4425b59291c70cb0a3a84cc64c2b6647
mschrode <>
2012-10-11 18:49:21 +0000

    produce collection of good vertices

--- python/goodPrimaryVertexFilter_cfi.py
+++ python/goodPrimaryVertexFilter_cfi.py
@@ -1,13 +1,20 @@
-## $Id: goodPrimaryVertexFilter_cfi.py,v 1.1 2012/05/24 11:33:04 mschrode Exp $
+## $Id: goodPrimaryVertexFilter_cfi.py,v 1.2 2012/05/24 14:50:55 mschrode Exp $
 ##
 ## Standard requirement for a good primary vertex as recommended in
 ## https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookCollisionsDataAnalysis#Analysis_of_the_processed_data
 
 import FWCore.ParameterSet.Config as cms
+
 goodPrimaryVertexFilter = cms.EDFilter(
-    "GoodVertexFilter",
-    vertexCollection = cms.InputTag('offlinePrimaryVertices'),
-    minimumNDOF = cms.uint32(4) ,
-    maxAbsZ = cms.double(24),
-    maxd0 = cms.double(2) 
+    "VertexSelector",
+    src = cms.InputTag("offlinePrimaryVertices"),
+    cut = cms.string("!isFake && ndof > 4 && abs(z) <= 24 && position.Rho <= 2"),
+    filter = cms.bool(True)
+    )
+
+goodPrimaryVertices = cms.EDFilter(
+    "VertexSelector",
+    src = cms.InputTag("offlinePrimaryVertices"),
+    cut = cms.string("!isFake && ndof > 4 && abs(z) <= 24 && position.Rho <= 2"),
+    filter = cms.bool(False)
     )
