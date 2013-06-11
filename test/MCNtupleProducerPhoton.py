d06f736b6947b1ec79552c0f0a357bcff574d8fe
telenz <>
2013-06-11 11:52:23 +0000

    final version for JER

--- test/MCNtupleProducerPhoton.py
+++ test/MCNtupleProducerPhoton.py
@@ -23,18 +23,19 @@ process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
 process.load('RecoBTag/Configuration/RecoBTag_cff')
 
-process.GlobalTag.globaltag = 'START53_V7G::All' 	
+process.GlobalTag.globaltag = 'START53_V22::All'
 
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(	
     '/store/mc/Summer12_DR53X/G_Pt-15to3000_TuneZ2_Flat_8TeV_pythia6/AODSIM/PU_S10_START53_V7A-v1/00000/EE4EEC78-4D0E-E211-BC0C-00266CF33288.root'
+#    '/store/mc/Summer12_DR53X/G_Pt-15to3000_TuneZ2_Flat_8TeV_pythia6/AODSIM/PU_S10_START53_V7A-v1/00000/507F6985-C70E-E211-9D0F-00266CF330D8.root'
     )
                             )
 
                             
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(4000) )  # number of events 
+    input = cms.untracked.int32(100) )  # number of events 
 
 process.options = cms.untracked.PSet(
     SkipEvent = cms.untracked.vstring('ProductNotFound'),
@@ -55,16 +56,19 @@ process.load("Calibration.CalibTreeMaker.CalibTreeMaker_cff")
 process.calibTreeMakerAK5FastPF.ECALDeadCellBEFilterModuleName = cms.InputTag("EcalDeadCellBoundaryEnergyFilter")
 process.calibTreeMakerAK5FastPF.ECALDeadCellTPFilterModuleName = cms.InputTag("EcalDeadCellTriggerPrimitiveFilter")
 process.calibTreeMakerAK5FastPF.WritePhotons = True
+
 process.calibTreeMakerAK5FastPF.TreeName = "GammaJetTree"
 process.calibTreeMakerAK5PFCHS.ECALDeadCellBEFilterModuleName = cms.InputTag("EcalDeadCellBoundaryEnergyFilter")
 process.calibTreeMakerAK5PFCHS.ECALDeadCellTPFilterModuleName = cms.InputTag("EcalDeadCellTriggerPrimitiveFilter")
 process.calibTreeMakerAK5PFCHS.WritePhotons = True
+
 process.calibTreeMakerAK5PFCHS.TreeName = "GammaJetTree"
 
-process.calibTreeMakerAK7PF.ECALDeadCellBEFilterModuleName = cms.InputTag("EcalDeadCellBoundaryEnergyFilter")
-process.calibTreeMakerAK7PF.ECALDeadCellTPFilterModuleName = cms.InputTag("EcalDeadCellTriggerPrimitiveFilter")
-process.calibTreeMakerAK7PF.WritePhotons = True
-process.calibTreeMakerAK7PF.TreeName = "GammaJetTree"
+process.calibTreeMakerAK7FastPF.ECALDeadCellBEFilterModuleName = cms.InputTag("EcalDeadCellBoundaryEnergyFilter")
+process.calibTreeMakerAK7FastPF.ECALDeadCellTPFilterModuleName = cms.InputTag("EcalDeadCellTriggerPrimitiveFilter")
+process.calibTreeMakerAK7FastPF.WritePhotons = True
+
+process.calibTreeMakerAK7FastPF.TreeName = "GammaJetTree"
 process.calibTreeMakerAK7PFCHS.ECALDeadCellBEFilterModuleName = cms.InputTag("EcalDeadCellBoundaryEnergyFilter")
 process.calibTreeMakerAK7PFCHS.ECALDeadCellTPFilterModuleName = cms.InputTag("EcalDeadCellTriggerPrimitiveFilter")
 process.calibTreeMakerAK7PFCHS.WritePhotons = True
@@ -88,7 +92,7 @@ process.pData = cms.Path(process.filterSequence *
                          process.ak5PFCHSJetsBtag *
                          process.calibTreeMakerAK5PFCHS *
                          process.ak7PFJetsBtag *
-                         process.calibTreeMakerAK7PF *
+                         process.calibTreeMakerAK7FastPF *
                          process.ak7PFCHSJetsBtag *
                          process.calibTreeMakerAK7PFCHS
                          )
